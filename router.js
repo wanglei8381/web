4ce8ccec41a4871a054c757d77219f64665038bb
junru.chen <junru.chen@zymobi.com>
2016-03-24 10:04:16 +0800

    24

--- router.js
+++ router.js
@@ -25,24 +25,24 @@ router.get('/notification/:id', routeMap.news.notificationDetail);
 router.get('/mynote', routeMap.note.mynoteList);
 router.get('/noteAdd', routeMap.note.noteAdd);
 router.get('/note', routeMap.note.noteList);
-router.get('/noteDetail', routeMap.note.noteDetail);
-router.get('/noteApproval', routeMap.note.noteApproval);
+router.get('/noteDetail/:id', routeMap.note.noteDetail);
+router.get('/noteApproval/:id', routeMap.note.noteApproval);
 
 //企业招聘(我的职位列表/职位上传/企业招聘列表/我的职位详情/职位详情/职位编辑)
 router.get('/myjob', routeMap.job.myjobList);
 router.get('/jobAdd', routeMap.job.jobAdd);
 router.get('/job', routeMap.job.jobList);
-router.get('/myjobDetail', routeMap.job.myjobDetail);
-router.get('/jobDetail', routeMap.job.jobDetail);
-router.get('/jobEdit', routeMap.job.jobEdit);
+router.get('/myjobDetail/:id', routeMap.job.myjobDetail);
+router.get('/jobDetail/:id', routeMap.job.jobDetail);
+router.get('/jobEdit/:id', routeMap.job.jobEdit);
 
 //资源共享(我的资源列表/资源上传/资源共享/我的资源详情/资源详情/资源编辑)
 router.get('/myresource', routeMap.resource.myresourceList);
 router.get('/resourceAdd', routeMap.resource.resourceAdd);
 router.get('/resource', routeMap.resource.resourceList);
-router.get('/myresourceDetail', routeMap.resource.myresourceDetail);
-router.get('/resourceDetail', routeMap.resource.resourceDetail);
-router.get('/resourceEdit', routeMap.resource.resourceEdit);
+router.get('/myresourceDetail/:id', routeMap.resource.myresourceDetail);
+router.get('/resourceDetail/:id', routeMap.resource.resourceDetail);
+router.get('/resourceEdit/:id', routeMap.resource.resourceEdit);
 
 //个人中心
 router.get('/personCenter', routeMap.person.personCenter);
